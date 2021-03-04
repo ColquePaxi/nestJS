f7ef64bd34efb200100c17c79ac2aa7a31588872
VALTERP <valter.paxi@ext.hdi.com.br>
2021-03-04 18:57:03 -0300

    User Repository provider configured

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { User } from './user.entity';
+import { UserDto } from './dto/user.dto';
+import { USER_REPOSITORY } from '../../core/constants';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(@Inject(USER_REPOSITORY) private readonly userRepository: typeof User) { }
+
+    // Como DTO não tá rolando, mas deveria funcionar
+    // async create(user: UserDto): Promise<User> {
+    async create(user: User): Promise<User> {
+        return await this.userRepository.create<User>(user);
+    }
+
+    async findOneByEmail(email: string): Promise<User> {
+        return await this.userRepository.findOne<User>({ where: { email } });
+    }
+
+    async findOneById(id: number): Promise<User> {
+        return await this.userRepository.findOne<User>({ where: { id } });
+    }
+}
